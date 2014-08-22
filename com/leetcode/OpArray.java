889f35e344ffb53d124cccc92b301d0871095558
Niuwj <niuwj2011@gmail.com>
2014-08-22 15:58:36 +0800

    add array

---
+++ com/leetcode/OpArray.java
@@ -0,0 +1,44 @@
+
+package com.leetcode;
+
+
+//对数组的一些操作
+public class OpArray {
+	
+	
+//Given two sorted integer arrays A and B, merge B into A as one sorted array.
+	public void merge(int A[], int m, int B[], int n) {
+		//算法：从后往前合并
+		if( A==null || B==null || m+n<=0){
+			return;
+		}
+		
+		int pa = m-1, pb = n-1;
+		
+		
+		int pnew = m+n-1;
+		
+		while(pa>=0 && pb>=0){
+			if(A[pa]>B[pb]){
+				A[pnew] = A[pa];
+				pa--;
+				pnew--;
+			}else {
+				A[pnew] = B[pb];
+				pb--;
+				pnew--;
+			}
+		}
+		while(pa>=0){
+			A[pnew] = A[pa];
+			pa--;
+			pnew--;
+		}
+		while(pb>=0){
+			A[pnew] = B[pb];
+			pb--;
+			pnew--;
+		}
+		return ;
+    }
+}
