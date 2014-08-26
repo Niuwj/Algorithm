ae4b96011ddbe917ddb1bd09d36812d65e1b92b4
Niuwj <niuwj2011@gmail.com>
2014-08-26 23:57:08 +0800

    add reorder ，int 回文

--- com/leetcode/Main.java
+++ com/leetcode/Main.java
@@ -22,7 +22,7 @@ public class Main {
 		l2.next = y2;
 		y2.next =y3;
 		y3.next = y4;
-		y4.next = y5;
+//		y4.next = y5;
 
 //		//result = AddTwoNumbers.addTwoNumbers(l1,l2);
 //		MergeTwoSortedList mts = new MergeTwoSortedList();
@@ -34,23 +34,26 @@ public class Main {
 //		ListNode p = llc.detectCycle(l1);
 //		System.out.println(p.val);
 //		System.out.println(llc.detectC(l1).val);
-//		ListOpetation lo = new ListOpetation();
+		ListOpetation lo = new ListOpetation();
 //		ListNode result = lo.insertionSortList(l2);
 //		result.print();
 //		result = lo.partition(l2, 6);
-//		l2.print();
+		l2.print();
 //		result = lo.reverseList(l2);
 //		result = lo.reverseBetween(l2, 2, 5);
 //		result.print();
 //		ReverseInt rInt = new ReverseInt();
-//		System.out.println(rInt.reverse(100003));
+//		System.out.println(rInt.reverse(1000034));
+		lo.reorderList(l2);
+		l2.print();
 		
-		
-		LRUCache lru = new LRUCache(1);
-		lru.set(2, 1);
-		System.out.println(lru.get(2));
-		lru.set(3, 2);
-		System.out.println(lru.get(2));
-		System.out.println(lru.get(3));
+//		LRUCache lru = new LRUCache(1);
+//		lru.set(2, 1);
+//		System.out.println(lru.get(2));
+//		lru.set(3, 2);
+//		System.out.println(lru.get(2));
+//		System.out.println(lru.get(3));
+		String s = new String("21");
+		int x = Integer.parseInt(s);
 	}
 }
