9e0f265cefb7272c4900ded02b0112b64675d8ee
Niuwj <niuwj2011@gmail.com>
2014-09-13 15:05:15 +0800

    try git change

--- com/leetcode/Sort.java
+++ com/leetcode/Sort.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import sort.BubbleSort;
+
 
 
 public class Sort {
@@ -96,5 +98,11 @@ public class Sort {
 		for(int i=0; i<result.length; i++){
 			System.out.print(result[i]+" ");
 		}
+	}
+	
+	//冒泡排序
+	public void BubbleSort(int[] num){
+		
+		
 	}
 }
