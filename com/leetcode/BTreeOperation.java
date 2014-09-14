f4c1c49fe82779f27773e86fa257e074e382a58f
Niuwj <niuwj2011@gmail.com>
2014-09-14 21:04:42 +0800

    implement

--- com/leetcode/BTreeOperation.java
+++ com/leetcode/BTreeOperation.java
@@ -1,8 +1,11 @@
 package com.leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -413,9 +416,21 @@ For this problem, a height-balanced binary tree is defined as a binary tree in w
 	  * Given a binary tree, find its minimum depth.
 
 The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+递归！！
 	  */
 	 public int minDepth(TreeNode root) {
-	        
+		 if(root==null){
+			 return 0;
+		 }
+		 if(root.left==null && root.right==null){
+			 return 1;
+		 }else if (root.left==null) {
+			return minDepth(root.right)+1;
+		 }else if (root.right==null) {
+			return minDepth(root.left)+1;
+		 }else {
+			return Math.min(minDepth(root.left),minDepth(root.right))+1;
+		 }		 
 	 }
 	 
 	 /*
@@ -425,6 +440,17 @@ The minimum depth is the number of nodes along the shortest path from the root n
 The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 	  */
 	 public int maxDepth(TreeNode root) {
-		 
+		 if(root==null){
+			 return 0;
+		 }
+		 if(root.left==null && root.right==null){
+			 return 1;
+		 }else if (root.left==null) {
+			return maxDepth(root.right)+1;
+		 }else if (root.right==null) {
+			return maxDepth(root.left)+1;
+		 }else {
+			return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+		 }
 	 }
 }
