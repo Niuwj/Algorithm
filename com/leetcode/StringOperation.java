410609a04720c93fb0bc72f4b3e67bc6ae39bcdd
Niuwj <niuwj2011@gmail.com>
2014-09-19 23:18:36 +0800

    valid number

--- com/leetcode/StringOperation.java
+++ com/leetcode/StringOperation.java
@@ -75,6 +75,29 @@ For the purpose of this problem, we define empty string as valid palindrome.
         return strs[0];
     }
 	
-	
+	/*
+	 * Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
+	 */
+	public boolean isNumber(String s) {		
+		//正则方法
+		if(s.trim().isEmpty()){  
+            return false;  
+        }  
+        String regex = "[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?";  
+        if(s.trim().matches(regex)){  
+            return true;  
+        }else{  
+            return false;  
+        }  
+        
+    }
 	
 }
