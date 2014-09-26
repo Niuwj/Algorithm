b66cf1b5190341404de324353c1edeaefe52cd1d
Niuwj <niuwj2011@gmail.com>
2014-09-26 22:45:39 +0800

    最长非递减子序列，百度笔试题

---
+++ baidu/Bishi.java
@@ -0,0 +1,19 @@
+package baidu;
+
+public class Bishi {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	/*最长非递减子序列
+	 * 题：求一个一维数组arr[i]中的最长递增子序列的长度，如在序列1，-1，2，-3，4，-5，6，-7中，最长递增子序列长度为4，可以是1，2，4，6，也可以是-1，2，4，6。
+	 */
+	
+	
+	
+	
+}
