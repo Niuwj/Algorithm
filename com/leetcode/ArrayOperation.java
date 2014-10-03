c1b0b8e131e978d6935fed1d2af9daad800279f5
Niuwj <niuwj2011@gmail.com>
2014-10-03 21:42:07 +0800

    set matrix zeros

--- com/leetcode/ArrayOperation.java
+++ com/leetcode/ArrayOperation.java
@@ -5,7 +5,91 @@ import java.util.List;
 
 //对数组的一些操作
 public class ArrayOperation {
+	/*
+	 * Set Matrix Zeroes 
+	 * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
 
+click to show follow up.
+
+Follow up:
+Did you use extra space?
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+	 */
+	public void setZeroes(int[][] matrix) {
+        //用第0行和第0列来保存数据
+		
+		int row = matrix.length;
+		if(row==0){
+			return;
+		}
+		int col = matrix[0].length;
+		if(col==0){
+			return;
+		}
+						
+		//1.store the 0 information in the first row and first col
+		boolean r0 = false;
+		boolean c0 = false;
+		
+		for(int i=0; i<col; i++){
+			if(matrix[0][i]==0){
+				r0 = true;
+				break;
+			}
+		}
+		
+		for(int j=0; j<row; j++){
+			if(matrix[j][0]==0){
+				c0 = true;
+				break;
+			}
+		}
+		
+		//2.store 0 into the first row and first col\
+		for(int i=1; i<row; i++){
+			for(int j=1; j<col; j++){
+				if(matrix[i][j]==0){
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+				}
+			}
+		}
+		
+		//3.traverse 0 row and 0 col and set  0
+		for(int i=1; i<col; i++){
+			if(matrix[0][i]==0){
+				for(int j=0;j<row;j++){
+					matrix[j][i] = 0;
+				}
+			}
+		}
+		for(int i=1; i<row; i++){
+			if(matrix[i][0]==0){
+				for(int j=0; j<col; j++){
+					matrix[i][j] = 0;
+				}
+			}
+		}
+		
+		//4.the orgin 0 row and 0 col
+		if(r0){
+			for(int i=0;i<col;i++){
+				matrix[0][i] = 0;
+			}
+		}
+		if(c0){
+			for(int i=0;i<row;i++){
+				matrix[i][0] = 0;
+			}
+		}
+		return;
+    }
+	
+	
+	
+	
 	/*
 	 * climbing stairs
 	 * You are climbing a stair case. It takes n steps to reach to the top.
