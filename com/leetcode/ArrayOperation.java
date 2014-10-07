438fdaefa71ffa559ac7e39af2000e5ccc714533
Niuwj <niuwj2011@gmail.com>
2014-10-07 23:30:07 +0800

    count and say

--- com/leetcode/ArrayOperation.java
+++ com/leetcode/ArrayOperation.java
@@ -8,6 +8,30 @@ import java.util.Map;
 //对数组的一些操作
 public class ArrayOperation {
 	
+	
+	
+	/*
+	 * count and say
+	 * The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth sequence.
+
+Note: The sequence of integers will be represented as a string.
+	 */
+	public String countAndSay(int n) {
+        
+    }
+	
+	
+	
+	
+	
+	
+	
 	/*
 	 * gas station
 	 * 
