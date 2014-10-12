dae5283e83d57c5bae86d91a005e9d18c282c1da
Niuwj <niuwj2011@gmail.com>
2014-10-12 21:11:52 +0800

    sum root to leaf numbers

--- com/leetcode/BTreeOperation.java
+++ com/leetcode/BTreeOperation.java
@@ -7,6 +7,47 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class BTreeOperation {
+	
+	
+	
+	/*
+	 * Sum Root to Leaf Numbers
+	 * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+For example,
+
+    1
+   / \
+  2   3
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+
+Return the sum = 12 + 13 = 25.
+
+
+	 */
+	
+	public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+	
+	public int dfs(TreeNode root, int sum){
+		if(root==null){
+			return 0;
+		}
+		if(root.left==null && root.right==null){
+			return sum*10 + root.val;
+		}
+		return dfs(root.left, sum*10+root.val)+dfs(root.right, sum*10+root.val);		
+	}
+	
+	
+	
+	
 	/*
 	 * Populating Next Right Pointers in Each Node 
 	 * Given a binary tree
@@ -102,7 +143,7 @@ If you notice carefully in the flattened tree, each node's right child points to
 			return;
 		}		
 		
-		//左子树插入root和root.right之间
+		//寻找左子树最右节点，将左子树插入root和root.right之间
 		TreeNode p = root.left;
 		while(p.right!=null){
 			p= p.right;
