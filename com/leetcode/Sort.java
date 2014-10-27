33aa983855b1e52aa2ff6aaaea0cf884d15528de
Niuwj <niuwj2011@gmail.com>
2014-10-27 21:57:14 +0800

    sort colors

--- com/leetcode/Sort.java
+++ com/leetcode/Sort.java
@@ -3,6 +3,59 @@ package com.leetcode;
 
 
 public class Sort {
+	/*
+	 * Sort Colors
+	 * Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note:
+You are not suppose to use the library's sort function for this problem.
+
+click to show follow up.
+
+Follow up:
+A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+
+Could you come up with an one-pass algorithm using only constant space?
+	 */
+	public void sortColors(int[] A) {
+		//双向指针，red（0），blue（2）		
+		int len = A.length;
+		if(len==1){
+			return;
+		}
+		int red = 0;
+		int blue = len-1;
+		while(red<=blue && A[red]==0){
+			red++;
+		}
+		while(blue>=red && A[blue]==2){
+			blue--;
+		}
+		int i = red;
+		while(i<=blue){
+			if(A[i]==0 && i>red){
+				int t = A[i];
+				A[i] = A[red];
+				A[red] = t;
+				red++;
+				continue;
+			}
+			if (A[i]==2) {
+				int t = A[i];
+				A[i] = A[blue];
+				A[blue] = t;
+				blue--;
+				continue;
+			}
+			i++;
+		}
+	}
+	
+	
+	
 	
 	/*
 	 * First Missing Positive 
